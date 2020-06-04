1301fd9448d3bfbcce47d5b69d871925db7afaa8
muhammadhuzaifa023 <64376908+muhammadhuzaifa023@users.noreply.github.com>
2020-06-04 15:48:41 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    autoplay:1000,
+    responsive:{
+        0:{
+            items:3
+        },
+        600:{
+            items:5
+        },
+        2000:{
+            items:5
+        }
+    }
+})
